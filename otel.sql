f9bca1bb36c8221aa16c53f1918c1850916599ca
Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>
2025-01-15 20:23:44 +0545

    type / input table definitions
    Signed-off-by: Abhinav Gyawali <22275402+abhizer@users.noreply.github.com>

--- otel.sql
+++ otel.sql
@@ -1,12 +1,102 @@
+CREATE TYPE KeyValue AS (
+    key VARCHAR,
+    value VARIANT
+);
+
+CREATE TYPE Event AS (
+    timeUnixNano VARCHAR,
+    name VARCHAR,
+    attributes KeyValue ARRAY
+);
+
+CREATE TYPE Span AS (
+    traceId VARCHAR,
+    spanId VARCHAR,
+    traceState VARCHAR,
+    parentSpanId VARCHAR,
+    flags BIGINT,
+    name VARCHAR,
+    kind INT,
+    startTimeUnixNano VARCHAR,
+    endTimeUnixNano VARCHAR,
+    attributes KeyValue ARRAY,
+    events Event ARRAY
+);
+
+CREATE TYPE Scope AS (
+    name VARCHAR,
+    version VARCHAR,
+    attributes KeyValue ARRAY
+);
+
+CREATE TYPE ScopeSpans AS (
+    scope Scope,
+    spans Span ARRAY
+);
+
+CREATE TYPE Resource AS (
+    attributes KeyValue ARRAY
+);
+
+CREATE TYPE ResourceSpans AS (
+    resource Resource,
+    scopeSpans ScopeSpans ARRAY
+);
+
+CREATE TYPE Metric AS (
+    name VARCHAR,
+    description VARCHAR,
+    unit VARCHAR,
+    data VARIANT,
+    metadata KeyValue ARRAY
+);
+
+CREATE TYPE ScopeMetrics AS (
+    scope Scope,
+    metrics Metric ARRAY
+);
+
+CREATE TYPE ResourceMetrics AS (
+    resource Resource,
+    scopeMetrics ScopeMetrics ARRAY
+);
+
+CREATE TYPE LogRecords AS (
+    attributes KeyValue ARRAY,
+    timeUnixNano VARCHAR,
+    observedTimeUnixNano VARCHAR,
+    severityNumber INT,
+    severityText VARCHAR,
+    flags INT4,
+    traceId VARCHAR,
+    spanId VARCHAR,
+    eventName VARCHAR,
+    body VARIANT
+);
+
+CREATE TYPE ScopeLogs AS (
+    scope Scope,
+    logRecords LogRecords ARRAY
+);
+
+CREATE TYPE ResourceLogs AS (
+    resource Resource,
+    scopeLogs ScopeLogs ARRAY
+);
+
 -- Input table that ingests resource spans from the collector.
 CREATE TABLE otel_traces (
-    resourceSpans VARIANT 
+    resourceSpans ResourceSpans ARRAY
 ) WITH ('append_only' = 'true');
 
 CREATE TABLE otel_logs (
-    resourceLogs VARIANT
+    resourceLogs ResourceLogs ARRAY
 ) WITH ('append_only' = 'true');
 
 CREATE TABLE otel_metrics (
-    resourceMetrics VARIANT
+    resourceMetrics ResourceMetrics ARRAY
 ) WITH ('append_only' = 'true');
+
+CREATE MATERIALIZED VIEW metrics AS SELECT resourceMetrics FROM otel_metrics;
+CREATE MATERIALIZED VIEW traces AS SELECT resourceSpans FROM otel_traces;
+CREATE MATERIALIZED VIEW logs AS SELECT resourceLogs FROM otel_logs;
